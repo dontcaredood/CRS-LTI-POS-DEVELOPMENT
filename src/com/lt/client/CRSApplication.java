c077d268f4ea3bc142a0c1fa44c205e6b4b1cbe2
santhosh <santhoshsan187@gmail.com>
2021-09-14 14:44:21 +0530

    Login and ViewCourses function commit

--- src/com/lt/client/CRSApplication.java
+++ src/com/lt/client/CRSApplication.java
@@ -6,35 +6,28 @@ import com.lt.business.ProfessorInterfaceImpl;
 
 public class CRSApplication {
 	public static void main(String[] args){
-		int input = 0;
-		while(input<4){
-			System.out.println("Course Registration System");
-			System.out.println("1.Sign Up\n2.Login\n3.Update Password\n4.Exit");
-			Scanner scan = new Scanner(System.in);
-			int roleSelection = scan.nextInt();
-			
-				switch(roleSelection){
-				case 1:
-					System.out.println("Sign Up");
-					break;
-				case 2:
-					ProfessorInterfaceImpl professor = new ProfessorInterfaceImpl();
-					professor.login();
-					break;
-				case 3:
-					System.out.println("Update Password");
-					break;
-				case 4:
-					System.out.println("Logged out!");
-					input = 5;
-					break;
-				default:
-					System.out.println("Please choose the valid user!");
-					input = 5;
-					break;
-				
-			}
-		}
+		System.out.println("Course Registration System");
+		System.out.println("1.Sign Up\n2.Login\n3.Update Password\n4.Exit");
+		Scanner scan = new Scanner(System.in);
+		int roleSelection = scan.nextInt();
 		
+			switch(roleSelection){
+			case 1:
+				System.out.println("Sign Up");
+				break;
+			case 2:
+				ProfessorInterfaceImpl professor = new ProfessorInterfaceImpl();
+				professor.login();
+				break;
+			case 3:
+				System.out.println("Update Password");
+				break;
+			case 4:
+				System.out.println("Logged out!");
+				break;
+			default:
+				System.out.println("Please choose the valid user!");
+			
+		}
 	}
 }
